a39fde050cac524b80dac5e96eb94a9f8c0d3588
FEAR939 <rogronin8877@gmail.com>
2025-01-24 20:25:47 +0100

    moved header stuff higher up

--- main.ts
+++ main.ts
@@ -7,6 +7,15 @@ import routes from "./routes.js";
 app.use(express.json());
 app.use(express.text({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
+// Handler for logging requests and CORS Origin
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", [
+    "Authorization",
+    "Content-Type",
+  ]);
+  next();
+});
 
 const PORT: number = 5000;
 
@@ -18,16 +27,6 @@ const pool = mariadb.createPool({
 });
 
 routes(app, pool);
-
-// Handler for logging requests and CORS Origin
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Headers", [
-    "Authorization",
-    "Content-Type",
-  ]);
-  next();
-});
 
 app.listen(PORT, () => {
   console.log("Server running on Port: %s", PORT);
