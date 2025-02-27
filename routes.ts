35d4839c136c07f764945f341ae0e171fdcda615
FEAR939 <rogronin8877@gmail.com>
2025-02-27 19:43:59 +0100

    renamed key in object

--- routes.ts
+++ routes.ts
@@ -239,15 +239,16 @@ export default function (app: Hono, conn: SQL) {
     try {
       const decoded = jwt.verify(token, "your-secret-key");
       const activityObj = c.req.json();
+      console.log(activityObj);
       if (
         !activityObj.time ||
-        !activityObj.date ||
+        !activityObj.day ||
         !activityObj.month ||
         !activityObj.year
       )
         throw new Error("Crucial data is missing!");
 
-      await conn`INSERT INTO watch_activity (id, time, date, month, year) VALUES (${(<any>decoded).userId}, ${activityObj.time}, ${activityObj.date}, ${activityObj.month}, ${activityObj.year}) ON CONFLICT(id, date, month, year) DO UPDATE SET time = watch_activity.time + excluded.time;`;
+      await conn`INSERT INTO watch_activity (id, time, date, month, year) VALUES (${(<any>decoded).userId}, ${activityObj.time}, ${activityObj.day}, ${activityObj.month}, ${activityObj.year}) ON CONFLICT(id, date, month, year) DO UPDATE SET time = watch_activity.time + excluded.time;`;
 
       return c.text("", 200);
     } catch (error) {
